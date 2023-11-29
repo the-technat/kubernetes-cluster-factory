f08872c4f2e243d215fe856e500d179bccf4ed77
Nathanael Liechti <technat@technat.ch>
2023-11-29 17:10:11 +0100

    feat: configure outputs

--- eks-terraform/outputs.tf
+++ eks-terraform/outputs.tf
@@ -1,7 +1,19 @@
+output "name" {
+  value = var.name
+}
+output "region" {
+  value = var.region
+}
+output "aws_iam_user" {
+  value = aws_iam_user.eks_admin.name
+}
 output "aws_accces_key_id" {
-	value = aws_iam_access_key.eks_admin.id
+  value = aws_iam_access_key.eks_admin.id
+}
+output "aws_accces_secret_key" {
+  value = nonsensitive(aws_iam_access_key.eks_admin.secret)
 }
 
-output "aws_accces_secret_key" {
-	value = nonsensitive(aws_iam_access_key.eks_admin.secret)
-}
+
+
+
