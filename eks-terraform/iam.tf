96db647ad6c8ca2245e042f068af7e9fcbf5fe99
Nathanael Liechti <technat@technat.ch>
2023-12-01 09:52:54 +0100

    fix: don't expose sensitive credentials to actions output

--- eks-terraform/iam.tf
+++ eks-terraform/iam.tf
@@ -1,3 +1,8 @@
+resource "aws_iam_role" "cluster_admin" {
+  name               = "EKSClusterAdmin"
+  assume_role_policy = data.aws_iam_policy_document.cluster_admin_assume.json
+}
+
 resource "aws_iam_role_policy_attachment" "account_admin" {
   role       = aws_iam_role.cluster_admin.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
@@ -5,33 +10,12 @@ resource "aws_iam_role_policy_attachment" "account_admin" {
   depends_on = [aws_iam_user.eks_admin]
 }
 
-resource "aws_iam_role" "cluster_admin" {
-  name               = "EKSClusterAdmin"
-  assume_role_policy = data.aws_iam_policy_document.cluster_admin_assume.json
-}
-
-resource "aws_iam_user_login_profile" "eks_admin" {
-  user = aws_iam_user.eks_admin.name
-}
-
 data "aws_iam_policy_document" "cluster_admin_assume" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
       type        = "AWS"
-      identifiers = [aws_iam_user.eks_admin.arn]
+      identifiers = ["*"]
     }
   }
-  depends_on = [aws_iam_user.eks_admin]
 }
-
-resource "aws_iam_user" "eks_admin" {
-  name = "eks_admin_${var.name}"
-
-  tags = local.tags
-}
-
-resource "aws_iam_access_key" "eks_admin" {
-  user = aws_iam_user.eks_admin.name
-}
-
--- eks-terraform/outputs.tf
+++ eks-terraform/outputs.tf
@@ -4,22 +4,11 @@ output "name" {
 output "region" {
   value = var.region
 }
-output "iam_user" {
-  value = aws_iam_user.eks_admin.name
-}
-output "iam_user_password" {
-  value = aws_iam_user_login_profile.eks_admin.password
-}
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
-output "aws_accces_key_id" {
-  value = aws_iam_access_key.eks_admin.id
+output "intructions" {
+  value = <<EOT
+   As soon as you can assume the EKSClusterAdmin role, you are all good
+ EOT
 }
-output "aws_accces_secret_key" {
-  value = nonsensitive(aws_iam_access_key.eks_admin.secret)
-}
-
-
-
-
